b007c4d9c95640ba02df13dd12f9a35a4ba7818c
Tanu-Singh412 <stanu4singh@gmail.com>
2026-04-10 16:58:56 +0530

    payment

--- src/layouts/projectTables/data/projectsTableData.js
+++ src/layouts/projectTables/data/projectsTableData.js
@@ -199,7 +199,7 @@ export default function useProjectData() {
         ),
 
         client: <MDTypography variant="caption">{p.clientName}</MDTypography>,
-        Total: <MDTypography variant="caption"> ₹ {totalPaid}</MDTypography>,
+        Total: <MDTypography variant="caption"> ₹ {p.totalAmount}</MDTypography>,
 
         payment: (
           <MDBox
