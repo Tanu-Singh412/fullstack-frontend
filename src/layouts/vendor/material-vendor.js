3973393f2fda5facf199420470f94686a96b260f
Tanu-Singh412 <stanu4singh@gmail.com>
2026-04-22 16:53:25 +0530

    MM

--- src/layouts/vendor/material-vendor.js
+++ src/layouts/vendor/material-vendor.js
@@ -14,8 +14,6 @@ import { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import Box from "@mui/material/Box";
-import Chip from "@mui/material/Chip";
 
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
